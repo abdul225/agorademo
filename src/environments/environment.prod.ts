e50dff299138ac645cefcd829beb67a14df08b26
Azeem <azeemoshaan007@gmail.com>
2022-02-26 18:35:08 +0530

    hiding env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  agora: {
+    appId: '9e17cc4e397e4d288d27abc2824354ae',
+    appCertificate: '2883fd5d6b24492593d5d126ed17ea36',
+    channelName: 'demo',
+  }
 };

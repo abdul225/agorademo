3403ffe5d688988add7f7f34636011cccb1fb716
abdul225 <azeemoshaan007@gmail.com>
2022-02-26 18:41:17 +0530

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  agora: {
-    appId: '9e17cc4e397e4d288d27abc2824354ae',
-    appCertificate: '2883fd5d6b24492593d5d126ed17ea36',
-    channelName: 'demo',
-  }
-};
